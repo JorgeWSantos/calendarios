e10e2db29623860d20995c58f4411e562668c338
JorgeWS <jorgews.dev@gmail.com>
2025-05-24 20:31:03 -0300

    feat: 🎸 Adicionará menu padrão

---
+++ src/menu.ts
@@ -0,0 +1,34 @@
+export default [
+  { name: 'Calendários', link: '#' },
+  { name: 'Resultados', link: '#' },
+  {
+    name: 'Ranking',
+    link: '#',
+    submenu: [
+      { name: 'Geral', link: '#' },
+      { name: 'Competidor', link: '#' },
+      { name: 'Animal', link: '#' },
+      { name: 'Reprodutor', link: '#' },
+      { name: 'Reprodutora', link: '#' },
+      { name: 'Avô', link: '#' },
+      { name: 'Proprietário', link: '#' },
+      { name: 'Criador', link: '#' },
+      { name: 'Premiação', link: '#' },
+    ],
+    open_submenu: false,
+  },
+  {
+    name: 'Campeonatos',
+    link: '#',
+    submenu: [
+      { name: 'Teste', link: '#' },
+      { name: 'TTTEEEEE', link: '#' },
+    ],
+    open_submenu: false,
+  },
+  { name: 'Eventos', link: '#' },
+  { name: 'Premiações', link: '#' },
+  { name: 'Awards', link: '#' },
+  { name: 'Financeiro', link: '#' },
+  { name: 'Acesso restrito', link: '#' },
+];
