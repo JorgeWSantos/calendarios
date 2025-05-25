0e8cc7ab9407539955f86f281d026f15210b7811
JorgeWS <jorgews.dev@gmail.com>
2025-05-25 16:19:30 -0300

    build: 🏠 Fará melhoria para visualizar erros

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets')
-    }
-  }
-})
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
+});
