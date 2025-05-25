a58e67192586a3c54a89c5e571bf655505b5beb7
JorgeWS <jorgews.dev@gmail.com>
2025-05-25 16:19:08 -0300

    feat: 🎸 Adicionará error boundary, permitindo validar o erro com mais facilidade

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { ErrorBoundary } from './ErrorBoundary';
 import { GlobalStyle } from './global.ts';
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GlobalStyle />
-    <App />
-  </StrictMode>,
-)
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>
+);
